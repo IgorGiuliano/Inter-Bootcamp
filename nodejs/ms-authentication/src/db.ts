1833bb6714402b7afefc2942520044bb3f847eb1
IgorGiuliano <igor.andrade07@gmail.com>
2022-01-25 18:40:54 -0300

    Update

--- nodejs/ms-authentication/src/db.ts
+++ nodejs/ms-authentication/src/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from 'pg';
 
-const connectionString = ;
+const connectionString = 'postgres://ijkkafkd:67aF0Lpe6SQNFhUgQWwVzmtc0qm1twN8@kesavan.db.elephantsql.com/ijkkafkd';
 
 const db = new Pool({ connectionString });
 
